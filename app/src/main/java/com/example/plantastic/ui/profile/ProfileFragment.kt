fc5bdb9e480c5089158535042506f234a18636cb
Shaneil Kapadia <shaneilkapadia@Kapptunes.local>
2023-11-28 04:15:30 -0800

    [PLAN-20] Added all UI components for Profile Fragment

--- app/src/main/java/com/example/plantastic/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/plantastic/ui/profile/ProfileFragment.kt
@@ -5,12 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 import com.example.plantastic.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+    private lateinit var saveButton: Button
+    private lateinit var foodPreferencesList: TextView
+    private lateinit var foodPreferencesEditText: EditText
+    private lateinit var dietaryRestrictionsSpinner: Spinner
+    private lateinit var activityPreferenceList: TextView
+    private lateinit var activityPreferencesEditText: EditText
+    private lateinit var profileViewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,8 +29,43 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        profileViewModel = ProfileViewModel()
+
+        foodPreferencesList = binding.foodPreferencesList
+        foodPreferencesEditText = binding.foodPreferencesInput
+        profileViewModel.foodPreferences.observe(viewLifecycleOwner) {
+            foodPreferencesList.text = it
+        }
+
+        dietaryRestrictionsSpinner = binding.dietaryRestrictionsInput
+        profileViewModel.dietaryRestrictionIndex.observe(viewLifecycleOwner){
+            dietaryRestrictionsSpinner.setSelection(it)
+        }
+
+        activityPreferenceList = binding.activityPreferencesList
+        activityPreferencesEditText = binding.activityPreferencesInput
+        profileViewModel.activityPreferences.observe(viewLifecycleOwner) {
+            activityPreferenceList.text = it
+        }
+
+        saveButton = binding.profileSaveButton
+        saveButton.setOnClickListener {
+            profileViewModel.foodPreferences.value = foodPreferencesEditText.text.toString()
+            profileViewModel.activityPreferences.value = activityPreferencesEditText.text.toString()
+            profileViewModel.dietaryRestrictionIndex.value = dietaryRestrictionsSpinner.selectedItemId.toInt()
+            println("sdot ProfileFragment:saveButton.setOnClickListener:55: ${dietaryRestrictionsSpinner.selectedItemId.toInt()}")
+        }
+
+
         val root: View = binding.root
         return root
+    }
+
+    private fun updateProfileViewModel() {
+        updateFoodPreferences()
+    }
+
+    private fun updateFoodPreferences() {
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/plantastic/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/plantastic/ui/profile/ProfileViewModel.kt
@@ -1,7 +1,38 @@
 package com.example.plantastic.ui.profile
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.sql.Types
+import java.util.Calendar
 
 class ProfileViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private val _foodPreferences = MutableLiveData<String>().apply {
+        value = "None"
+    }
+    val foodPreferences = _foodPreferences
+
+    private val _dietaryRestrictionIndex = MutableLiveData<Int>().apply {
+        value = 0
+    }
+    val dietaryRestrictionIndex = _dietaryRestrictionIndex
+
+    private val _activityPreferences = MutableLiveData<String>().apply{
+        value = "None"
+    }
+    val activityPreferences = _activityPreferences
+
+    private val _availability = MutableLiveData<Array<IntArray>>().apply {
+        var monday: IntArray = intArrayOf(0, 1440)
+        var tuesday: IntArray = intArrayOf(0, 1440)
+        var wednesday: IntArray = intArrayOf(0, 1440)
+        var thursday: IntArray = intArrayOf(0, 1440)
+        var friday: IntArray = intArrayOf(0, 1440)
+        var saturday: IntArray = intArrayOf(0, 1440)
+        var sunday: IntArray = intArrayOf(0, 1440)
+        value = arrayOf(monday,tuesday,wednesday,thursday,friday,saturday,sunday)
+    }
+    val availability = _availability
+
 }
